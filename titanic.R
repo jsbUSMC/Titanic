7e7e5d199a3b3c58562c59cdee033c2440677c4e
John Bissonette <john.bissonette@rochester.edu>
2017-12-16 21:22:14 -0500

    initial commit

---
+++ titanic.R
@@ -0,0 +1,13 @@
+# Author: John S. Bissonette
+# Date: 16 Dec. 2017
+# This R script works with the Titanic dataset, provided by Kaggle. The overall
+# purpose of the script is to attempt to predict whether or not a passenger in
+# the test set survived the sinking of the ship, based upon the training data
+# provided in the training set. It is an exercise in prediction models and
+# machine learning.
+
+# Set working directory, and import data files from CSV
+setwd("~/projects/kaggle/titanic")
+library(readr)
+train <- read_csv("~/projects/kaggle/titanic/data/train.csv", stringsAsFactors = FALSE)
+test <- read_csv("~/projects/kaggle/titanic/data/test.csv", stringsAsFactors = FALSE)
